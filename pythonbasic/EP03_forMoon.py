6b3f62ba1968774f6be48ac150b12152c976d645
syjang <tpdud7500@naver.com>
2024-10-30 21:20:58 +0900

    ep03 commit

---
+++ pythonbasic/EP03_forMoon.py
@@ -0,0 +1,16 @@
+# range  함수 for문
+# for num in range(1,101):
+#     print(num)
+#
+# print("end")
+
+# list for문
+# fruits = ["사과","바나나","딸기"]
+#
+# for i in fruits:
+#     print(i)
+
+#dictionary for문
+student_scores = {"철수":90,"영희":82,"민수":95}
+for name, score in student_scores.items():
+    print(name,score)
